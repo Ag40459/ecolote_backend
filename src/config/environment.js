62ab694dc32b14b4d16480bf01c6aa4c1ba32615
Agenor <agenortorres10@gmail.com>
2025-05-12 20:11:03 -0300

    ok

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const environment = {
     port: process.env.PORT || 3001,
     supabaseUrl: process.env.SUPABASE_URL,
-    supabaseServiceKey: process.env.SUPABASE_SERVICE_KEY
+    supabaseServiceKey: process.env.SUPABASE_KEY
 };
 
 module.exports = environment;
